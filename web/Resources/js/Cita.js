624d71913c5207c908c9e8114c46ceb918930854
Digitality 5 <Digitality 5@Zbox>
2015-11-18 20:07:26 -0600

    Cambios en validación de cita

--- web/Resources/js/Cita.js
+++ web/Resources/js/Cita.js
@@ -13,6 +13,27 @@ attr('data-bvalidator', 'required,required');
     //Opciones del validador
     var optionsRed = { 
         classNamePrefix: 'bvalidator_bootstraprt_', 
+        onBeforeValidate: function(element, action){
+            
+            
+            var idEmpleado = $('#dgplusbellebundle_cita_empleado').val();
+            var fechaCita= $('#dgplusbellebundle_cita_fechaCita').val();
+            var horaCita = $('#dgplusbellebundle_cita_horaCita_hour').val()+":"+$('#dgplusbellebundle_cita_horaCita_minute').val();
+                    //alert(idEmpleado);
+            $.getJSON(Routing.generate('get_existeCita', { idempleado: idEmpleado,fecha:fechaCita,hora:horaCita}), 
+                function(data) {
+                    if(data.regs.length!=0){
+                        var mensaje = "";
+                        mensaje += "<div class='row'><div class='col-md-12'><b><p class='text-left'>Error...</p></b></div></div>";
+                        mensaje += "<div class='row'><hr></div>";
+                        mensaje += "<div class='row'><div class='col-md-12'>Ya existe una cita programada para ese doctor en ese dia y esa hora</div></div>"
+                        bootbox.alert(mensaje, function() {
+
+                        });
+                    }
+            });
+                        
+        },
         lang: 'es'
     };
  
