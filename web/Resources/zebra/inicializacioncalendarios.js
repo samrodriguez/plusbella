be329f2d4fb33a49dc64ae5064005041f72fe90b
Digitality 5 <Digitality 5@Zbox>
2015-11-28 09:47:23 -0600

    Formato de encabezados y corrección de opciones editar y desactivar en usuarios

--- web/Resources/zebra/inicializacioncalendarios.js
+++ web/Resources/zebra/inicializacioncalendarios.js
@@ -5,6 +5,15 @@
  */
 
 
+        $('#dgplusbellebundle_cita_fechaCita').Zebra_DatePicker({
+            months:['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+            days: ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
+            direction:true,
+            show_clear_date:false,
+            show_select_today: "Hoy",
+            default_position: "below",
+            offset:[-142,40]
+        });
         
         
         $('#fecha').Zebra_DatePicker({
